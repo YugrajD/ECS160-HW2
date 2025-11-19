bcd0c57b6cad565672bb1d1ce009a4d02ee26d8b
YugrajD <139527332+YugrajD@users.noreply.github.com>
2025-11-19 14:23:10 -0800

    implemented annotation checker, and endpoint annotation

--- microservice-framework/src/main/java/com/ecs160/Launcher.java
+++ microservice-framework/src/main/java/com/ecs160/Launcher.java
@@ -3,6 +3,19 @@ package com.ecs160;
 class Launcher {
 
     public boolean launch(int port) {
+        ClassLoaderHelper classLoaderHelper = new ClassLoaderHelper();
+        List<Class<?>> classes = classLoaderHelper.listClassesInAllJarsInOwnDirectory();
+        for (Class<?> clazz : classes) {
+            if (clazz.isAnnotationPresent(Microservice.class)) {
+                Object instance = clazz.getDeclaredConstructor().newInstance();
+                for (Method method : clazz.getDeclaredMethods()) {
+                    if (method.isAnnotationPresent(Endpoint.class)) {
+                        Endpoint endpoint = method.getAnnotation(Endpoint.class);
+                        String url = endpoint.url();
+                    }
+                }
+            }
+        }
         return false;
     }
 }

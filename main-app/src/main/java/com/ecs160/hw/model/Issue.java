aa6446e190ff71eae11f30216dff6347178f013a
YugrajD <139527332+YugrajD@users.noreply.github.com>
2025-11-24 08:06:00 -0800

    added stuff to repo and issue

--- main-app/src/main/java/com/ecs160/hw/model/Issue.java
+++ main-app/src/main/java/com/ecs160/hw/model/Issue.java
@@ -1,4 +1,12 @@
 package com.ecs160.hw.model;
 
+import com.ecs160.persistence.annotations.Id;
+import com.ecs160.persistence.annotations.PersistableField;
+
 public class Issue {
+    @Id
+    String issueID;
+
+    @PersistableField
+    String description;
 }
--- main-app/src/main/java/com/ecs160/hw/model/Repo.java
+++ main-app/src/main/java/com/ecs160/hw/model/Repo.java
@@ -1,4 +1,18 @@
 package com.ecs160.hw.model;
 
+import com.ecs160.persistence.annotations.Id;
+import com.ecs160.persistence.annotations.PersistableField;
+import com.ecs160.persistence.annotations.PersistableObject;
+
+@PersistableObject
 public class Repo {
+    @Id
+    String name;
+
+    @PersistableField
+    String url;
+
+    @PersistableField
+    String issues;
+
 }

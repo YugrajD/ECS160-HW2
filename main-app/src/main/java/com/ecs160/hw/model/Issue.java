72a523ce230e58c874df30b45d3a96ccf6d70d85
YugrajD <139527332+YugrajD@users.noreply.github.com>
2025-11-28 20:57:56 -0800

    forgot to add part a

--- main-app/src/main/java/com/ecs160/hw/model/Issue.java
+++ main-app/src/main/java/com/ecs160/hw/model/Issue.java
@@ -2,7 +2,9 @@ package com.ecs160.hw.model;
 
 import com.ecs160.persistence.annotations.Id;
 import com.ecs160.persistence.annotations.PersistableField;
+import com.ecs160.persistence.annotations.PersistableObject;
 
+@PersistableObject
 public class Issue {
     @Id
     public String id;
